088199a414228eb6f152dd0272df982ad16f692b
yanira2 <ruth.cortez@puzzle.tech>
2023-04-26 13:03:02 -0600

    Martes avance del curso Udemy

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Controllers
+{
+
+    // https://localhost:portnumber/api/regions
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalksDbContext dbContext;
+
+        public RegionsController(NZWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+
+        // GET ALL REGIONS
+        // GET: https://localhost:portnumber/api/regions
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = dbContext.Regions.ToList();
+
+            return Ok(regions);
+        }
+
+
+        // GET SINGLE REGION (Get Region By ID)
+        // GET: https://localhost:portnumber/api/regions/{id}
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute]Guid id)
+        {
+
+            var region = dbContext.Regions.Find(id);
+            
+            if(region == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(region);
+
+        }
+
+    }
+}
