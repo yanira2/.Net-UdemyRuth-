88107d5a25c28faa51a9a741316141f6faa46f24
yanira2 <ruth.cortez@puzzle.tech>
2023-04-28 11:34:32 -0600

    Seccion 7 Seeding - Filtering Data

--- NZWalks.API/Controllers/WalksController.cs
+++ NZWalks.API/Controllers/WalksController.cs
@@ -38,11 +38,11 @@ namespace NZWalks.API.Controllers
         }
 
         // GET Walks
-        // GET: /api/walks
+        // GET: /api/walks?filterOn=Name&filterQuery=Track
         [HttpGet]
-        public async Task<IActionResult> GetAll()
+        public async Task<IActionResult> GetAll([FromQuery] string? filterOn, [FromQuery] string? filterQuery)
         {
-            var walksDomainModel = await walkRepository.GetAllAsync();
+            var walksDomainModel = await walkRepository.GetAllAsync(filterOn, filterQuery);
 
             // Map Domain Model to DTO
             return Ok(mapper.Map<List<WalkDto>>(walksDomainModel));
