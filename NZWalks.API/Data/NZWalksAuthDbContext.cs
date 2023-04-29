60ebb3c5c6a8a388287c4100a9520c2e095e75a7
yanira2 <ruth.cortez@puzzle.tech>
2023-04-28 19:32:04 -0600

    Seccion 8 - Seeding Roles

--- NZWalks.API/Data/NZWalksAuthDbContext.cs
+++ NZWalks.API/Data/NZWalksAuthDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace NZWalks.API.Data
@@ -9,5 +10,34 @@ namespace NZWalks.API.Data
         {
 
         }
-    }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var readerRoleId = "61099e63-f7b6-4e22-8c5e-af3921c6537a";
+            var writerRoleId = "e25892ff-f325-447c-aab0-c90524ba9529";
+
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = readerRoleId,
+                    ConcurrencyStamp = readerRoleId,
+                    Name = "Reader",
+                    NormalizedName = "Reader".ToUpper()
+                },
+                new IdentityRole
+                {
+                    Id = writerRoleId,
+                    ConcurrencyStamp = writerRoleId,
+                    Name = "Writer",
+                    NormalizedName = "Writer".ToUpper()
+                }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
+    }   
 }
